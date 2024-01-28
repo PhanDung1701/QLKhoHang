691efe0414a7c57f44bd85fef5b6029fd93c9d91
PhanDung1701 <141790127+PhanDung1701@users.noreply.github.com>
2024-01-28 13:26:53 +0700

    Add project files.

---
+++ MVC_QLKHO/Controllers/LoginController.cs
@@ -0,0 +1,44 @@
+﻿using MVC_QLKHO.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_QLKHO.Controllers
+{
+    public class LoginController : Controller
+    {
+        //
+        // GET: /Login/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(NhanVien ob)
+        {
+            if (string.IsNullOrWhiteSpace(ob.MaNV) || string.IsNullOrWhiteSpace(ob.MatKhau))
+            {
+                ModelState.AddModelError("", "Vui lòng nhập đầy đủ tên đăng nhập và mật khẩu.");
+                return View();
+            }
+
+            var loggedInUser = LoginHelper.Login(ob);
+
+            if (loggedInUser == null)
+            {
+                ModelState.AddModelError("", "Sai tên đăng nhập hoặc mật khẩu.");
+                return View();
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+        public ActionResult Logout()
+        {
+            LoginHelper.Logout();
+            return RedirectToAction("Index");
+        }
+	}
+}
